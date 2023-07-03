fc71a2d9068600fa48f9e74ea54d68668cd1ee1f
erlanggapranansa <erlanggapranansa@gmail.com>
2023-07-03 17:39:26 +0700

    mengolah button baru di data siswa

---
+++ Unit1.pas
@@ -0,0 +1,132 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, frxClass, frxDBSet, DB, ZAbstractRODataset, ZAbstractDataset,
+  ZDataset, ZAbstractConnection, ZConnection, ComCtrls, StdCtrls, Grids,
+  DBGrids;
+
+type
+  TForm1 = class(TForm)
+    lbl2: TLabel;
+    lbl3: TLabel;
+    lbl4: TLabel;
+    lbl5: TLabel;
+    lbl6: TLabel;
+    lbl7: TLabel;
+    lbl8: TLabel;
+    lbl9: TLabel;
+    lbl10: TLabel;
+    lbl11: TLabel;
+    lbl12: TLabel;
+    lbl13: TLabel;
+    lbl15: TLabel;
+    lbl1: TLabel;
+    dbgrd1: TDBGrid;
+    btn1: TButton;
+    btn2: TButton;
+    btn3: TButton;
+    btn5: TButton;
+    edt2: TEdit;
+    edt3: TEdit;
+    edt4: TEdit;
+    edt5: TEdit;
+    edt6: TEdit;
+    edt7: TEdit;
+    dtp1: TDateTimePicker;
+    edt8: TEdit;
+    edt9: TEdit;
+    edt10: TEdit;
+    edt11: TEdit;
+    edt12: TEdit;
+    btn4: TButton;
+    edt1: TEdit;
+    btn6: TButton;
+    con1: TZConnection;
+    zqry1: TZQuery;
+    ds1: TDataSource;
+    frxrprt1: TfrxReport;
+    frxdbdtst1: TfrxDBDataset;
+    procedure btn1Click(Sender: TObject);
+    procedure posisiawal;
+    procedure bersih;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  id : string;
+
+implementation
+
+{$R *.dfm}
+
+procedure TForm1.bersih;
+begin
+  edt1.clear;
+  edt2.clear;
+  edt3.clear;
+  edt4.clear;
+  edt5.clear;
+  dtp1.CleanupInstance;
+  edt6.clear;
+  edt7.clear;
+  edt8.clear;
+  edt9.clear;
+  edt10.clear;
+  edt11.clear;
+
+end;
+
+procedure TForm1.btn1Click(Sender: TObject);
+begin
+btn1.Enabled := False;
+  btn2.Enabled := True;
+  btn3.Enabled := False;
+  btn4.Enabled := False;
+  btn5.Enabled := True;
+  edt1.Enabled := True;
+  edt2.Enabled := True;
+  edt3.Enabled := True;
+  edt4.Enabled := True;
+  edt5.Enabled := True;
+  edt6.Enabled := True;
+  edt7.Enabled := True;
+  edt8.Enabled := True;
+  edt9.Enabled := True;
+  edt10.Enabled := True;
+  edt11.Enabled := True;
+  edt12.Enabled := True;
+  dtp1.Enabled := True;
+end;
+
+procedure TForm1.posisiawal;
+begin
+  bersih;
+  btn1.Enabled := True;
+  btn2.Enabled := False;
+  btn3.Enabled := False;
+  btn4.Enabled := False;
+  btn5.Enabled := False;
+  edt1.Enabled := False;
+  edt2.Enabled := False;
+  edt3.Enabled := False;
+  edt4.Enabled := False;
+  edt5.Enabled := False;
+  edt6.Enabled := False;
+  edt7.Enabled := False;
+  edt8.Enabled := False;
+  edt9.Enabled := False;
+  edt10.Enabled := False;
+  edt11.Enabled := False;
+  edt12.Enabled := False;
+  dtp1.Enabled := False
+
+end;
+
+end.
